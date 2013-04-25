ac85db7f0e8072192972ff664866e028b94292bc
Eytan Anjel <anjel@usc.edu>
2013-04-24 19:07:03 -0700

    fixed minor problems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ SendEmail::Application.configure do
   config.assets.debug = true
   
   config.action_mailer.delivery_method=:smtp
-  config.action_mailer.perform_delivering = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
